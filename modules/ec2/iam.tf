1d1f85a248db46c78784431cad0beaa29bb8e75d
Sridevi <parimisridevi586@gmail.com>
2025-09-07 21:04:28 +1000

    created a new iam role

--- modules/ec2/iam.tf
+++ modules/ec2/iam.tf
@@ -1,17 +1,15 @@
 resource "aws_iam_role" "main" {
-  name = "${var.name}-${var.env}-role"
-
-  assume_role_policy = jsonencode(
-    {
-    "Version": "2012-10-17",
-    "Statement": [
+    name = "${var.name}-${var.env}-role"
+    assume_role_policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
         {
-            "Effect": "Allow",
-            "Principal": {
-                "Service": "ec2.amazonaws.com"
-            },
-            "Action": "sts:AssumeRole"
+        "Effect" : "Allow",
+        "Principal" : {
+            "Service" : "ec2.amazonaws.com"
+        },
+        "Action" : "sts:AssumeRole"
         }
-    ]
-})
+     ]
+    })
 }
