cdc615980d4377b6f08bde861cdb5f87da40a39f
Sridevi <parimisridevi586@gmail.com>
2025-10-04 20:09:29 +1000

    added nginx ingress controller

---
+++ modules/eks/helm.tf
@@ -0,0 +1,6 @@
+# this nginx ingress controller comes on top of load balancer and argoCD 
+resource "helm_release" "nginx_ingress" {
+  name       = "nginx-ingress-controller"
+  repository = "https://kubernetes.github.io/ingress-nginx"
+  chart      = "ingress-nginx"
+}
