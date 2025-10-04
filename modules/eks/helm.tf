99714ddcd5c801bc70364882a20729b09a491432
Sridevi <parimisridevi586@gmail.com>
2025-10-04 20:22:12 +1000

    added nginx ingress controller and a null resource to update config

--- modules/eks/helm.tf
+++ modules/eks/helm.tf
@@ -1,6 +1,13 @@
+resource "null_resource" "kubeconfig"{
+    depends_on  = [aws_eks_node_group.nodegroup_1]
+    provisioner "local-exec"{
+        command = "aws eks update-kubeconfig --name ${var.env} --region us-east-1"
+    }
+}
 # this nginx ingress controller comes on top of load balancer and argoCD 
 resource "helm_release" "nginx_ingress" {
+  depends_on = [null_resource.kubeconfig]
   name       = "nginx-ingress-controller"
   repository = "https://kubernetes.github.io/ingress-nginx"
-  chart      = "ingress-nginx"
+  chart      = "ingress-nginx" 
 }
