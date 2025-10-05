a3fe4dd5eaafd69b0d86f535eca7cb865f386d1d
Sridevi <parimisridevi586@gmail.com>
2025-10-05 19:39:44 +1100

    error fixing in helm

--- modules/eks/helm.tf
+++ modules/eks/helm.tf
@@ -1,24 +1,45 @@
-resource "null_resource" "kubeconfig"{
-  depends_on  = [
+#resource "null_resource" "kubeconfig"{
+# depends_on  = [
+#   aws_eks_cluster.main-cluster,
+#   aws_eks_node_group.nodegroup,
+#   aws_eks_addon.example,
+#   aws_eks_access_entry.access,
+#    aws_eks_access_policy_association.main
+#   ]
+# provisioner "local-exec"{
+#     command = <<EOT
+        
+#       aws eks update-kubeconfig --name ${var.env} --region us-east-1
+#       kubectl get svc || true
+#     EOT
+# }
+#}
+# this nginx ingress controller comes on top of load balancer and argoCD 
+#resource "helm_release" "nginx_ingress" { 
+# depends_on = [null_resource.kubeconfig]
+# name       = "nginx-ingress-controller"
+# repository = "https://kubernetes.github.io/ingress-nginx"
+#  chart      = "ingress-nginx" 
+#}
+
+# EKS Cluster Auth Data
+data "aws_eks_cluster" "eks" {
+  name = aws_eks_cluster.main-cluster.name
+}
+
+data "aws_eks_cluster_auth" "eks" {
+  name = aws_eks_cluster.main-cluster.name
+}
+resource "helm_release" "nginx_ingress" {
+  depends_on = [
     aws_eks_cluster.main-cluster,
     aws_eks_node_group.nodegroup,
     aws_eks_addon.example,
     aws_eks_access_entry.access,
     aws_eks_access_policy_association.main
-    ]
-  provisioner "local-exec"{
-      command = <<EOT
-        
-        aws eks update-kubeconfig --name ${var.env} --region us-east-1
-        kubectl get svc || true
-      EOT
-  }
-}
-# this nginx ingress controller comes on top of load balancer and argoCD 
-resource "helm_release" "nginx_ingress" {
-  
-  depends_on = [null_resource.kubeconfig]
+  ]
+
   name       = "nginx-ingress-controller"
   repository = "https://kubernetes.github.io/ingress-nginx"
-  chart      = "ingress-nginx" 
+  chart      = "ingress-nginx"
 }
--- provider.tf
+++ provider.tf
@@ -14,8 +14,15 @@ terraform {
     }
   }
 }
+#provider "helm" {
+#  kubernetes {
+#    config_path = "~/.kube/config"
+#  }
+#}
 provider "helm" {
   kubernetes {
-    config_path = "~/.kube/config"
+    host                   = data.aws_eks_cluster.eks.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority[0].data)
+    token                  = data.aws_eks_cluster_auth.eks.token
   }
 }
