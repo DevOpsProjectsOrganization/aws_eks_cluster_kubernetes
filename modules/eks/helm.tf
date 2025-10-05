88488bfb714ffb42ad74c4965ad7a26fc7b4b869
Sridevi <parimisridevi586@gmail.com>
2025-10-05 17:12:30 +1100

    depending on both node group and cluster creation

--- modules/eks/helm.tf
+++ modules/eks/helm.tf
@@ -1,12 +1,16 @@
 resource "null_resource" "kubeconfig"{
     depends_on  = [
       aws_eks_cluster.main-cluster,
-      aws_eks_node_group.nodegroup
+      aws_eks_node_group.nodegroup,
+      aws_eks_addon.example,
+      aws_eks_access_entry.access,
+      aws_eks_access_policy_association.main
       ]
     provisioner "local-exec"{
         command = <<EOT
           aws eks wait cluster-active --name ${var.env}
           aws eks update-kubeconfig --name ${var.env} --region us-east-1
+          sleep 500s
         EOT
     }
 }
