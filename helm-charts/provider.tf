e9654870369119beef78100f4a0567273743d7f6
Sridevi <parimisridevi586@gmail.com>
2025-10-06 11:37:40 +1100

    moved helm to a different folder in root

---
+++ helm-charts/provider.tf
@@ -0,0 +1,37 @@
+provider "aws" {
+    region = "us-east-1"
+}
+terraform {
+    backend "s3" {
+        bucket  = "terraform-bucket-85"
+        key     = "ecommerce_terraform/dev/terraform.tfstate"
+        region  = "us-east-1"
+    }
+    required_providers {
+    helm = {
+      source  = "hashicorp/helm"
+      version = "2.17.0"
+    }
+  }
+}
+#provider "helm" {
+#  kubernetes {
+#    config_path = "~/.kube/config"
+#  }
+#}
+data "aws_eks_cluster" "eks" {
+  name = module.eks.aws_eks_cluster.main-cluster.name
+}
+
+data "aws_eks_cluster_auth" "eks" {
+  name = module.eks.aws_eks_cluster.main-cluster.name
+}
+
+# --- Now configure Helm provider dynamically ---
+provider "helm" {
+  kubernetes {
+    host                   = data.aws_eks_cluster.eks.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority[0].data)
+    token                  = data.aws_eks_cluster_auth.eks.token
+  }
+}
