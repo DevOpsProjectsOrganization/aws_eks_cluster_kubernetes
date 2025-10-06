4372530ce03adc759793732d9b1ee190cd60bba4
Sridevi <parimisridevi586@gmail.com>
2025-10-06 21:01:51 +1100

    error fixing in helm

--- provider.tf
+++ provider.tf
@@ -15,12 +15,16 @@ terraform {
   }
 }
 
-provider "helm" {
- kubernetes {
-   config_path = "~/.kube/config"
- }
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.main-cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.main-cluster.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.main-cluster.token
 }
 
-provider "kubernetes" {
-  config_path = "~/.kube/config"
+provider "helm" {
+  kubernetes {
+    host                   = data.aws_eks_cluster.main-cluster.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.main-cluster.certificate_authority[0].data)
+    token                  = data.aws_eks_cluster_auth.main-cluster.token
+  }
 }
