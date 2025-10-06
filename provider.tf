16bfef5320424feb4b282f56f7308f2bd69c254f
Sridevi <parimisridevi586@gmail.com>
2025-10-06 12:57:22 +1100

    moved helm to a different folder in root

--- provider.tf
+++ provider.tf
@@ -37,3 +37,12 @@ provider "helm" {
     token                  = data.aws_eks_cluster_auth.eks.token
   }
 }
+
+provider "helm" {
+  alias = "eks"
+  kubernetes {
+    host                   = data.aws_eks_cluster.eks.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority[0].data)
+    token                  = data.aws_eks_cluster_auth.eks.token
+  }
+}
