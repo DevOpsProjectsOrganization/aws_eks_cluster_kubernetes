0a9180024636a66c4c77e1a23920f9e95d6fc015
Sridevi <parimisridevi586@gmail.com>
2025-10-06 15:59:08 +1100

    error fixing in helm

--- modules/eks/outputs.tf
+++ modules/eks/outputs.tf
@@ -1,7 +1,7 @@
 output "cluster_name" {
-  value = aws_eks_cluster.main.name
+  value = aws_eks_cluster.main-cluster.name
 }
 
 output "cluster_id" {
-  value = aws_eks_cluster.main.id
+  value = aws_eks_cluster.main-cluster.id
 }
