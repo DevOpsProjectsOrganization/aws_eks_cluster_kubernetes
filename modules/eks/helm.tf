abe0ac3377497a7a9967c65ef0303863910faa08
Sridevi <parimisridevi586@gmail.com>
2025-10-06 17:33:37 +1100

    error fixing in helm

---
+++ modules/eks/helm.tf
@@ -0,0 +1,15 @@
+resource "null_resource" "kubeconfig" {
+  depends_on = [aws_eks_cluster.main-cluster]
+
+  provisioner "local-exec" {
+    command = "aws eks update-kubeconfig --region us-east-1 --name ${aws_eks_cluster.main-cluster.name} --kubeconfig /home/ec2-user/kubeconfig_${aws_eks_cluster.main-cluster.name}"
+  }
+}
+# this nginx ingress controller comes on top of load balancer and argoCD 
+resource "helm_release" "nginx_ingress" {
+  depends_on = [null_resource.kubeconfig]
+  name       = "nginx-ingress-controller"
+  repository = "https://kubernetes.github.io/ingress-nginx"
+  chart      = "ingress-nginx"
+  create_namespace = true
+}
