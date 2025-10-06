5496db3c4c6d7b8e97dbd052f21b443af486757c
Sridevi <parimisridevi586@gmail.com>
2025-10-06 12:52:35 +1100

    moved helm to a different folder in root

--- provider.tf
+++ provider.tf
@@ -22,10 +22,12 @@ terraform {
 
 data "aws_eks_cluster" "eks" {
   name = module.eks["main"].cluster_name
+  depends_on = [module.eks]
 }
 
 data "aws_eks_cluster_auth" "eks" {
   name = module.eks["main"].cluster_name
+  depends_on = [module.eks]
 }
 
 provider "helm" {
