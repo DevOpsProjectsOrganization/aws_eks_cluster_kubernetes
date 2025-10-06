1f20054d2f1b897f235c5eae6d037865388fb13b
Sridevi <parimisridevi586@gmail.com>
2025-10-06 16:26:52 +1100

    error fixing in helm

--- helm-charts/helm.tf
+++ helm-charts/helm.tf
@@ -1,26 +1,13 @@
-#resource "null_resource" "kubeconfig"{
-# depends_on  = [
-#   aws_eks_cluster.main-cluster,
-#   aws_eks_node_group.nodegroup,
-#   aws_eks_addon.example,
-#   aws_eks_access_entry.access,
-#    aws_eks_access_policy_association.main
-#   ]
-# provisioner "local-exec"{
-#     command = <<EOT
-        
-#       aws eks update-kubeconfig --name ${var.env} --region us-east-1
-#       kubectl get svc || true
-#     EOT
-# }
-#}
+resource "null_resource" "kubeconfig"{
+ depends_on  = [module.eks]
+provisioner "local-exec"{
+     command = <<EOT        
+       aws eks update-kubeconfig --name ${var.env} --region us-east-1
+       echo "success"
+     EOT
+ }
+}
 # this nginx ingress controller comes on top of load balancer and argoCD 
-#resource "helm_release" "nginx_ingress" { 
-# depends_on = [null_resource.kubeconfig]
-# name       = "nginx-ingress-controller"
-# repository = "https://kubernetes.github.io/ingress-nginx"
-#  chart      = "ingress-nginx" 
-#}
 resource "helm_release" "nginx_ingress" {
   depends_on = [data.aws_eks_cluster.eks]
   name       = "nginx-ingress-controller"
--- provider.tf
+++ provider.tf
@@ -15,23 +15,8 @@ terraform {
   }
 }
 
-# Fetch the cluster info after module.eks is created
-data "aws_eks_cluster" "eks" {
-  name       = module.eks["main"].cluster_name
-  depends_on = [module.eks]  # ensures cluster exists first
-}
-
-data "aws_eks_cluster_auth" "eks" {
-  name       = module.eks["main"].cluster_name
-  depends_on = [module.eks]
-}
-
-# Helm provider using the above cluster info
 provider "helm" {
-  alias = "eks"
   kubernetes {
-    host                   = data.aws_eks_cluster.eks.endpoint
-    cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority[0].data)
-    token                  = data.aws_eks_cluster_auth.eks.token
+    config_path = "./kubeconfig_${module.eks["main"].cluster_name}"
   }
 }
