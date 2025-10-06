467a939d244fad351a6abeab8af00eaafd5c014c
Sridevi <parimisridevi586@gmail.com>
2025-10-06 17:44:11 +1100

    error fixing in helm

--- provider.tf
+++ provider.tf
@@ -7,16 +7,16 @@ terraform {
         key     = "ecommerce_terraform/dev/terraform.tfstate"
         region  = "us-east-1"
     }
-    #required_providers {
-    #helm = {
-    #  source  = "hashicorp/helm"
-    #  version = "2.17.0"
-    #}
-  #}
+    required_providers {
+    helm = {
+      source  = "hashicorp/helm"
+      version = "2.17.0"
+    }
+  }
 }
 
-#provider "helm" {
-# kubernetes {
-#   config_path = "~/.kube/config"
-# }
-#}
+provider "helm" {
+ kubernetes {
+   config_path = "~/.kube/config"
+ }
+}
