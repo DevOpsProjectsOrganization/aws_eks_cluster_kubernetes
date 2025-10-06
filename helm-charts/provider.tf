590ea3e142d63abdca8f8487c6c02ddd83c9fb73
Sridevi <parimisridevi586@gmail.com>
2025-10-06 11:45:06 +1100

    moved helm to a different folder in root

--- helm-charts/provider.tf
+++ helm-charts/provider.tf
@@ -19,19 +19,3 @@ provider "helm" {
     config_path = "~/.kube/config"
   }
 }
-#data "aws_eks_cluster" "eks" {
- #name = module.eks.aws_eks_cluster.main-cluster.name
-#}
-
-#data "aws_eks_cluster_auth" "eks" {
-# name = module.eks.aws_eks_cluster.main-cluster.name
-#}
-
-# --- Now configure Helm provider dynamically ---
-#provider "helm" {
-# kubernetes {
-#   host                   = data.aws_eks_cluster.eks.endpoint
-#   cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority[0].data)
-#   token                  = data.aws_eks_cluster_auth.eks.token
-# }
-#}
