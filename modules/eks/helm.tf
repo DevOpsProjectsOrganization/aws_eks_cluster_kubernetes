a5b7be818e2ff23f2222a4eab163f891e08dd621
Sridevi <parimisridevi586@gmail.com>
2025-10-09 20:34:36 +1100

    error fixing in helm

--- modules/eks/helm.tf
+++ modules/eks/helm.tf
@@ -1,9 +1,9 @@
 data "aws_eks_cluster" "main-cluster" {
-  name = aws_eks_cluster.main-cluster.name
+  name = "dev"
 }
 
 data "aws_eks_cluster_auth" "main-cluster" {
-  name = aws_eks_cluster.main-cluster.name
+  name = "dev"
 }
 
 provider "kubernetes" {
