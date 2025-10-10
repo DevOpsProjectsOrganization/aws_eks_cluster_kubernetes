4d81a3f2e1351348cdef0e0272ec54f429dad6f3
Sridevi <parimisridevi586@gmail.com>
2025-10-10 16:36:54 +1100

    seperated all te helm into a seperate folder outside eks

--- helm-charts/provider.tf
+++ helm-charts/provider.tf
@@ -1,20 +1,3 @@
-provider "aws" {
-  region = "us-east-1"
-}
-terraform {
-  backend "s3" {
-    bucket  = "terraform-bucket-85"
-    key     = "ecommerce_terraform/dev/terraform.tfstate"
-    region  = "us-east-1"
-  }
-  required_providers {
-    helm = {
-      source  = "hashicorp/helm"
-      version = "= 2.17.0"
-    }
-  }
-}
-
 provider "helm" {
   kubernetes {
     config_path = "~/.kube/config"
