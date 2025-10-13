934789658e02a0ceb2cd538e1010a5c5f57aef03
Sridevi <parimisridevi586@gmail.com>
2025-10-13 21:26:46 +1100

    added pod iam role

--- modules/eks/helm.tf
+++ modules/eks/helm.tf
@@ -27,3 +27,11 @@ resource "helm_release" "external-dns" {
   create_namespace  = true
   namespace         = "tools"
 }
+
+resource "helm_release" "argocd" {
+  depends_on      = [null_resource.kubeconfig, helm_release.nginx_ingress ]
+  name            = "argo-cd"
+  repository      = "https://github.com/argoproj/argo-cd"
+  create_namespace= true
+  namespace       = "tools"
+}
