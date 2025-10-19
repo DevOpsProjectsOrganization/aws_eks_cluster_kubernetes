d0a513b920e10a07231f7bc983c3181f7441bac7
Sridevi <parimisridevi586@gmail.com>
2025-10-19 20:13:32 +1100

    added vault token as secret

--- modules/eks/helm.tf
+++ modules/eks/helm.tf
@@ -65,6 +65,23 @@ resource "null_resource" "external-secret-store"{
         namespace: tools
       data:
         token: ${base64encode(var.vault_token)}
+      ---
+      apiVersion: external-secrets.io/v1
+      kind: ClusterSecretStore
+      metadata:
+        name: vault-backend
+        namespace: tools
+      spec:
+        provider:
+          vault:
+            server: "http://vault-internal.sdevops.shop:8200"
+            path: "roboshop-${var.env}"
+            version: "v2"
+            auth:
+              tokenSecretRef:
+                name: "vault-token"
+                key: "token"
+                namespace: tools
      
     EOK
     EOF
